1a07988ce274b0c39d2745a6ca65405aebee2d9c
Jason Dang <jason.dang@techtonic.com>
2018-10-02 13:52:58 -0600

    push Library Console templates

---
+++ js/book.js
@@ -0,0 +1,10 @@
+function Boo(title, author, numPages, pubDate ) {
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.pubDate = pubDate;
+};
+
+Book.prototype.editBook = function(oBook) {
+
+};
